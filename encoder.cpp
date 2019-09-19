e016b180b4e26f090ef196d724b44c63d343ab8a
Aaron Fishman <uk.afishman@gmail.com>
2019-09-19 07:25:26 -0700

    encode header file

--- encoder.cpp
+++ encoder.cpp
@@ -18,131 +18,12 @@ using namespace cv;
 
 #include <ctime>
 using namespace std;
+
 #include <chrono>
 using namespace std::chrono;
 
 #include "videoIO/VideoIO.h"
-
-// TODO: offload these to run/compiler-time settings file
-#define QUEUE_CAPACITY 64
-#define DISPLAY_SCALE 0.25
-#define FFMPEG_OPTIONS "-y -codec:v libx264 -preset ultrafast"
-#define WINDOW_NAME "Dalsa Monitor"
-
-class VideoEncoder 
-{
-    private:
-        bool _debug;
-        int writeCount;
-        VideoIO _writer;
-        boost::atomic<bool> _done;
-        boost::thread* _encoderThread;
-        boost::thread* _displayThread;
-        boost::lockfree::spsc_queue<cv::Mat, boost::lockfree::capacity<QUEUE_CAPACITY>> _queue;
-
-        // This mutex is used for synchronise communication with ffmpeg
-        std::condition_variable cv;
-        std::mutex cv_m;
-        std::mutex display_m;
-
-        /* ffmpeg worker thread */ 
-        void ffmpegWorker(void)
-        {
-            while (!_done) 
-            {
-                // Sleep while the queue is empty
-                std::unique_lock<std::mutex> lk(cv_m);
-                cv.wait(lk);
-
-                writeQueue();
-            }
-            writeQueue();
-        }
-
-        /* pipe all accumulated frames onto ffmpeg */ 
-        void writeQueue()
-        {
-            cv::Mat img;
-            while (_queue.pop(img))
-            {
-                // Pass to ffmpeg
-                // TODO: handle status
-                int status = _writer.WriteFrame(img);
-                logFrame();
-                img.release();
-            }
-        }
-
-        /* Log */ 
-        // TODO: use boost logging framework
-        void logFrame()
-        {
-            writeCount++;
-
-            if(!_debug)
-            {
-                return;
-            }
-
-            cout << "writeQueue() # " << writeCount << endl;
-            cout << "\tencoder queue size: " << _queue.read_available() << endl;
-        }
-
-        /* Display Thread */ 
-        cv::Mat displayImg;
-        void displayFrame(void)
-        {
-            while(!_done)
-            {
-                // Wait for next image
-                std::unique_lock<std::mutex> lk(cv_m);
-                cv.wait(lk);
-
-                // Lock the frame
-                std::lock_guard<std::mutex> lock(display_m);
-
-                // Display
-                if(!displayImg.empty()) imshow(WINDOW_NAME, displayImg);
-                int key = waitKey(1);
-
-                // Release Image
-                displayImg.release();
-            }
-        }
-
-    public:
-        VideoEncoder(char*, int, int, int, int, bool);
-
-        /* Add an image to the encoder queue */ 
-        int writeFrame(cv::Mat img)
-        {
-            while (!_queue.push(img));
-
-            // Pass to display
-            if(display_m.try_lock()) 
-            {
-                try {cv::resize(img, displayImg, cv::Size(), DISPLAY_SCALE, DISPLAY_SCALE);}
-                catch(...){}
-
-                display_m.unlock();
-            }
-
-            // Wake up the threads
-            cv.notify_all();
-
-            return 0;
-        }
-
-        /* Hangs until the encoder queue is processed */
-        int close()
-        {
-            _done = true;
-            _encoderThread->join();
-            _displayThread->join();
-
-            return 0;
-        }
-};
+#include "encoder.h"
 
 /* Prepare for frame encoding */ 
 VideoEncoder::VideoEncoder(char filename[], int width, int height, int framerate, int crf, bool debug=false)
@@ -165,5 +46,102 @@ VideoEncoder::VideoEncoder(char filename[], int width, int height, int framerate
     // TODO: Include option to disable this
     namedWindow(WINDOW_NAME, WINDOW_AUTOSIZE);
 }
+
+
+/* ffmpeg worker thread */ 
+void VideoEncoder::ffmpegWorker(void)
+{
+    while (!_done) 
+    {
+        // Sleep while the queue is empty
+        std::unique_lock<std::mutex> lk(cv_m);
+        cv.wait(lk);
+
+        writeQueue();
+    }
+    writeQueue();
+}
+
+/* pipe all accumulated frames onto ffmpeg */ 
+void VideoEncoder::writeQueue()
+{
+    cv::Mat img;
+    while (_queue.pop(img))
+    {
+        // Pass to ffmpeg
+        // TODO: handle status
+        int status = _writer.WriteFrame(img);
+        logFrame();
+        img.release();
+    }
+}
+
+/* Log */ 
+// TODO: use boost logging framework
+void VideoEncoder::logFrame()
+{
+    writeCount++;
+
+    if(!_debug)
+    {
+        return;
+    }
+
+    cout << "writeQueue() # " << writeCount << endl;
+    cout << "\tencoder queue size: " << _queue.read_available() << endl;
+}
+
+/* Display Thread */ 
+void VideoEncoder::displayFrame(void)
+{
+    while(!_done)
+    {
+        // Wait for next image
+        std::unique_lock<std::mutex> lk(cv_m);
+        cv.wait(lk);
+
+        // Lock the frame
+        std::lock_guard<std::mutex> lock(display_m);
+
+        // Display
+        if(!displayImg.empty()) imshow(WINDOW_NAME, displayImg);
+        int key = waitKey(1);
+
+        // Release Image
+        displayImg.release();
+    }
+}
+
+
+/* Add an image to the encoder queue */ 
+int VideoEncoder::writeFrame(cv::Mat img)
+{
+    while (!_queue.push(img));
+
+    // Pass to display
+    if(display_m.try_lock()) 
+    {
+        try {cv::resize(img, displayImg, cv::Size(), DISPLAY_SCALE, DISPLAY_SCALE);}
+        catch(...){}
+
+        display_m.unlock();
+    }
+
+    // Wake up the threads
+    cv.notify_all();
+
+    return 0;
+}
+
+/* Hangs until the encoder queue is processed */
+int VideoEncoder::close()
+{
+    _done = true;
+    _encoderThread->join();
+    _displayThread->join();
+
+    return 0;
+}
+
 
 #endif /* __ENCODER_CPP__ */
---
+++ encoder.h
@@ -0,0 +1,57 @@
+/*
+    A simple wrapper around the videoIO ffmpeg piper with a SPSC work queue
+    Thanks: http://www.boost.org/doc/libs/1_53_0/doc/html/lockfree/examples.html
+*/
+#include <boost/thread/thread.hpp>
+#include <boost/lockfree/spsc_queue.hpp>
+#include <boost/atomic.hpp>
+
+#include <opencv2/opencv.hpp>
+using namespace cv;
+
+#include <mutex>
+#include <condition_variable>
+
+#include <ctime>
+using namespace std;
+
+#include <chrono>
+using namespace std::chrono;
+
+// TODO: offload these to runtime settings file
+#define QUEUE_CAPACITY 64
+#define DISPLAY_SCALE 0.25
+#define FFMPEG_OPTIONS "-y -codec:v libx264 -preset ultrafast"
+#define WINDOW_NAME "Dalsa Monitor"
+
+class VideoEncoder 
+{
+    public:
+        VideoEncoder(char*, int, int, int, int, bool);
+        int writeFrame(cv::Mat img);
+        int close();
+
+    private:
+        bool _debug;
+        int writeCount;
+        VideoIO _writer;
+        boost::atomic<bool> _done;
+        boost::thread* _encoderThread;
+        boost::thread* _displayThread;
+        boost::lockfree::spsc_queue<cv::Mat, boost::lockfree::capacity<QUEUE_CAPACITY>> _queue;
+
+        // This mutex is used for synchronise communication with ffmpeg
+        std::condition_variable cv;
+        std::mutex cv_m;
+        std::mutex display_m;
+
+        void ffmpegWorker(void); // ffmpeg worker thread
+
+        void writeQueue(); // pipe all accumulated frames onto ffmpeg
+
+        void logFrame();
+
+        // Display Thread 
+        cv::Mat displayImg;  
+        void displayFrame(void);
+};
